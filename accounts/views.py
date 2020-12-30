8018c0bdd8f0bc7bcea7ad2aed07de471300d631
Marco Chacon <marco.chacon1796@gmail.com>
2020-12-30 17:12:05 -0600

    adding profile pics

--- accounts/views.py
+++ accounts/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpResponse
 from django.forms import inlineformset_factory
 from .models import *
-from .forms import OrderForm, CreateUserForm
+from .forms import *
 from .filters import OrderFilter
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
@@ -93,9 +93,23 @@ def userPage(request):
     })
 
 @login_required(login_url='login')
+@allowed_users(allowed_roles=['customer'])
+def accountSettings(request):
+    customer = request.user.customer
+    form = CustomerForm(instance=customer)
+    if request.method == "POST":
+        form = CustomerForm(request.POST, request.FILES, instance=customer)
+        if form.is_valid():
+            form.save()
+    return render(request, 'accounts/account_settings.html', {
+        'form': form
+    })
+
+@login_required(login_url='login')
 @allowed_users(allowed_roles=['admin'])
 def products(request):
     products = Product.objects.all()
+
     return render(request, 'accounts/products.html', 
     {
         'products': products
